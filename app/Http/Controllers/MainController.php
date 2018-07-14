bf7f699fc235e562f4654edaf817017338db0d1a
山崎一洋 <yamazakikazuhiro@yamazaki-mbp.local>
2018-07-14 15:59:15 +0900

    引数不足

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function receive(){
+    public function receive(Request $request){
         $data = $request->all();
         //get the user’s id
         $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
